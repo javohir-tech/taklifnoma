91783cc2ba75285467393b0a4a19da941673a4d5
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-08-08 11:36:08 +0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weddingDate = new Date('August 15, 2024 06:43:00').getTime();
+const weddingDate = new Date('August 18, 2024 06:43:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
